e533c536554cc84952963a847af92b92cd5a2a4e
Dawid Kacza <dawidkacza02@gmail.com>
2022-09-17 15:52:33 +0200

    Created basic desktop Grid layout

--- js/templates.js
+++ js/templates.js
@@ -1,5 +1,18 @@
 const templates = {
-    desktopFramework: '<h1>Desktop</h1>',
+    desktopFramework: `
+    <main class="desktop">
+    <div class="container">
+        <section class="date">DATE</section>
+        <section class="tasks">TASKS</section>
+        <div class="callendar">CALLENDAR</div>
+        <aside>ASIDE</aside>
+        <button class="add-task-btn">ADD TASK</button>
+    </div>
+    
+    <div class="add-task-modal"></div>
+    <div class="delete-task-modal"></div>
+
+    </main>`,
     mobileFramework: '<h1>Mobile</h1>',
 }
 export default templates;
